d075adb24aa3ce08232e6f7e08857050cc5bd8c8
RENAN CAMPOS TORRES <renanctorres7@gmail.com>
2021-01-08 17:02:10 -0300

    Criação de um SliverAppBar para que ao rolar a lista o AppBar diminui e se esconde.

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -37,55 +37,89 @@ class _HomeState extends State<Home> {
   }
 
   @override
+  void dispose() {
+    super.dispose();
+    getData();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-          appBar: AppBar(),
-          body: Container(
-            child: FutureBuilder<Repository>(
-              future: repository,
-              builder: (context, snapshot) {
-                print('###################### $snapshot');
 
-                if (snapshot.hasError) {
-                  return const Center(
-                    child: const Text('Erro na data'),
-                  );
-                }
-                switch (snapshot.connectionState) {
-                  case ConnectionState.waiting:
+          body: NestedScrollView(
+            headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled){
+              return <Widget> [
+                SliverAppBar(
+                  expandedHeight: 150,
+                  flexibleSpace: const FlexibleSpaceBar(
+                    title: ListTile(
+                      title: const Text("GitHub", style: TextStyle(
+                        fontSize: 36,
+                        fontWeight: FontWeight.w500
+                      ),
+                      ),
+                      subtitle: const Text("Repositórios", style: TextStyle(
+                        color: Colors.black38,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                      ),
+
+                      ),
+                    ),
+                    centerTitle: true,
+                  ),
+                )
+              ];
+            },
+            body: Container(
+              child: FutureBuilder<Repository>(
+                future: repository,
+                builder: (context, snapshot) {
+
+                  if (snapshot.hasError) {
                     return const Center(
-                      child: CircularProgressIndicator(),
+                      child: const Text('Erro na data'),
                     );
-                  case ConnectionState.none:
-                    return const Center(
-                      child: const Text('Erro'),
-                    );
-                  default:
-                    return ListView.builder(
-                        itemCount: 10,
-                        itemBuilder: (context, index) {
-                          String avatar = list[index].avatar;
-                          String name = list[index].name;
-                          // String url = snapshot.data.url;
-                          String repo = list[index].repo;
+                  }
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    case ConnectionState.none:
+                      return const Center(
+                        child: const Text('Erro'),
+                      );
+                    default:
+                      return ListView.builder(
+                          itemCount: 10,
+                          itemBuilder: (context, index) {
+                            String avatar = list[index].avatar;
+                            String name = list[index].name;
+                            // String url = snapshot.data.url;
+                            String repo = list[index].repo;
 
 
-                          return Card(
-                            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                            child: ListTile(
-                              leading: Container(
-                                child: Image.network(avatar),
+                            return InkWell(
+                              onTap: () {},
+                              child: Card(
+                                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                                child: ListTile(
+                                  leading: Container(
+                                    child: Image.network(avatar),
+                                  ),
+                                  title: Text(repo.toUpperCase()),
+                                  subtitle: Text("Autor: $name"),
+                                ),
                               ),
-                              title: Text("Repositório: $repo"),
-                              subtitle: Text("Autor: $name"),
-                            ),
-                          );
-                        }
-                    );
-                }
+                            );
+                          }
+                      );
+                  }
 
-              },
+                },
+              ),
             ),
           ),
         )
