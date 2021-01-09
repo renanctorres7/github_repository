3e4ae1968315adee38fd5716e92d90a5df85c6fc
RENAN CAMPOS TORRES <renanctorres7@gmail.com>
2021-01-08 21:06:43 -0300

    Configurado os botões para a exibição da lista com 10, 50 ou 100 items.

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -4,7 +4,6 @@ import 'package:github_repository/data/repository.dart';
 import 'package:http/http.dart' as http;
 import 'package:url_launcher/url_launcher.dart';
 
-
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
@@ -13,12 +12,13 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   Future<Repository> repository;
   var list = List<Repository>();
-
+  bool enable;
+  int items = 10;
 
   Future<Repository> getData() async {
     final response = await http.get('https://api.github.com/repositories');
 
-    if(response.statusCode == 200) {
+    if (response.statusCode == 200) {
       var decoded = jsonDecode(response.body);
       list = decoded.map<Repository>((e) {
         return Repository.fromJson(e);
@@ -29,22 +29,27 @@ class _HomeState extends State<Home> {
     }
   }
 
-   launchUrl(String url) async {
+  launchUrl(String url) async {
     if (await canLaunch(url)) {
       await launch(url);
     } else {
       throw "Could not launch url";
     }
-    }
+  }
 
+  isButtonDisabled(int count) {
+    setState(() {
+      enable = true;
+      items = count;
+    });
+  }
 
   @override
   void initState() {
-
     super.initState();
     getData();
     repository = getData();
-
+    enable = false;
   }
 
   @override
@@ -57,139 +62,147 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-
-          body: NestedScrollView(
-
-            headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled){
-              return <Widget> [
-                SliverAppBar(
-                  expandedHeight: 150,
-                  flexibleSpace: const FlexibleSpaceBar(
-                    title: ListTile(
-                      title: const Text("GitHub", style: TextStyle(
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              expandedHeight: 150,
+              flexibleSpace: const FlexibleSpaceBar(
+                title: ListTile(
+                  title: const Text(
+                    "GitHub",
+                    style: TextStyle(
                         fontSize: 36,
                         fontWeight: FontWeight.w500,
-                        color: Colors.white
-                      ),
-                      ),
-                      subtitle: const Text("Repositórios", style: TextStyle(
-                        color: Colors.grey,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w400,
-                      ),
-
-                      ),
-                    ),
-                    centerTitle: true,
+                        color: Colors.white),
                   ),
-                )
-              ];
-            },
-            body: Container(
-              child: FutureBuilder<Repository>(
-                future: repository,
-                builder: (context, snapshot) {
+                  subtitle: const Text(
+                    "Repositórios",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+                centerTitle: true,
+              ),
+            )
+          ];
+        },
+        body: Container(
+          child: FutureBuilder<Repository>(
+            future: repository,
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return const Center(
+                  child: const Text('Erro na data'),
+                );
+              }
+              switch (snapshot.connectionState) {
+                case ConnectionState.waiting:
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                case ConnectionState.none:
+                  return const Center(
+                    child: const Text('Erro'),
+                  );
+                default:
+                  final innerScrollController =
+                      PrimaryScrollController.of(context);
 
-                  if (snapshot.hasError) {
-                    return const Center(
-                      child: const Text('Erro na data'),
-                    );
-                  }
-                  switch (snapshot.connectionState) {
-                    case ConnectionState.waiting:
-                      return const Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    case ConnectionState.none:
-                      return const Center(
-                        child: const Text('Erro'),
-                      );
-                    default:
-                      final innerScrollController = PrimaryScrollController.of(context);
-
-                      return ListView(
-                        controller: innerScrollController,
+                  return ListView(
+                    controller: innerScrollController,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
                         children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.only(right: 8.0),
-                                child: RaisedButton(
-                                  color: Colors.white54,
-                                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-                                    onPressed: (){},
-                                  child: Text("10"),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(right: 8.0),
-                                child: RaisedButton(
-                                  color: Colors.white54,
-                                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-                                  onPressed: (){},
-                                  child: Text("50"),
-                                ),
-                              ),
-                              RaisedButton(
-
-                                color: Colors.white54,
-                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-                                onPressed: (){},
-                                child: Text("100"),
-                              )
-                            ],
-                          ),
-
-                          ClipRRect(
-                            borderRadius: BorderRadius.only(topLeft: Radius.circular(130)),
-                            child: Container(
-
-                              color: Colors.white,
-                              child: Container(
-                                padding: const EdgeInsets.only(top: 40),
-                                margin: const EdgeInsets.only(left: 40),
-                                child: ListView.builder(
-                                    shrinkWrap: true,
-                                    itemCount: 100,
-                                    itemBuilder: (context, index) {
-                                      String avatar = list[index].avatar;
-                                      String name = list[index].name;
-                                      String url = list[index].url;
-                                      String repo = list[index].repo;
-
-
-                                      return InkWell(
-                                        onTap: (){
-                                          launchUrl(url);
-                                        },
-                                        child: ListTile(
-                                          contentPadding: const EdgeInsets.all(10),
-                                          leading: CircleAvatar(
-                                            radius: 40,
-                                            backgroundImage: NetworkImage(avatar),
-                                          ),
-                                          title: Text(repo.toUpperCase()),
-                                          subtitle: Text("Autor: $name"),
-                                        ),
-                                      );
-                                    }
-                                ),
-                              ),
-
+                          Container(
+                            width: 70,
+                            padding: const EdgeInsets.only(right: 8.0),
+                            child: RaisedButton(
+                              color:
+                                  items == 10 ? Colors.white : Colors.white54,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5)),
+                              onPressed: () {
+                                isButtonDisabled(10);
+                              },
+                              child: Text("10"),
                             ),
                           ),
-
+                          Container(
+                            width: 70,
+                            padding: const EdgeInsets.only(right: 8.0),
+                            child: RaisedButton(
+                              color:
+                                  items == 50 ? Colors.white : Colors.white54,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5)),
+                              onPressed: () {
+                                isButtonDisabled(50);
+                              },
+                              child: Text("50"),
+                            ),
+                          ),
+                          Container(
+                            width: 70,
+                            child: RaisedButton(
+                              color:
+                                  items == 100 ? Colors.white : Colors.white54,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5)),
+                              onPressed: () {
+                                isButtonDisabled(100);
+                              },
+                              child: Text("100"),
+                            ),
+                          )
                         ],
-                      );
-                  }
+                      ),
+                      ClipRRect(
+                        borderRadius:
+                            BorderRadius.only(topLeft: Radius.circular(130)),
+                        child: Container(
+                          color: Colors.white,
+                          child: Container(
+                            padding: const EdgeInsets.only(top: 40),
+                            margin: const EdgeInsets.only(left: 40),
+                            child: ListView.builder(
+                                shrinkWrap: true,
+                                itemCount: items,
+                                itemBuilder: (context, index) {
+                                  String avatar = list[index].avatar;
+                                  String name = list[index].name;
+                                  String url = list[index].url;
+                                  String repo = list[index].repo;
 
-                },
-              ),
-            ),
+                                  return InkWell(
+                                    onTap: () {
+                                      launchUrl(url);
+                                    },
+                                    child: ListTile(
+                                      contentPadding: const EdgeInsets.all(10),
+                                      leading: CircleAvatar(
+                                        radius: 40,
+                                        backgroundImage: NetworkImage(avatar),
+                                      ),
+                                      title: Text(repo.toUpperCase()),
+                                      subtitle: Text("Autor: $name"),
+                                    ),
+                                  );
+                                }),
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+              }
+            },
           ),
-        )
-    );
+        ),
+      ),
+    ));
   }
 }
