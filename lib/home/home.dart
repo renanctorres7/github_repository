6705392c6846a751deb7177303b5b640b283af43
RENAN CAMPOS TORRES <renanctorres7@gmail.com>
2021-01-08 22:38:14 -0300

    Adicionado ícone

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -19,6 +19,7 @@ class _HomeState extends State<Home> {
   bool floatVisible = true;
 
 
+  // ignore: missing_return
   Future<Repository> getData() async {
     final response = await http.get('https://api.github.com/repositories');
 
